24d0865f089ff5e416d7371fc1c315043302295f
Heechan Park <phcphc82@gmail.com>
2021-10-02 09:11:35 +0900

    Add devbase

---
+++ devbase/devbase.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+TAG=devbase
+VER=18.04
+
+build() {
+  docker build -t $TAG:$VER --build-arg=version=$VER --build-arg=user=$USER --build-arg=uid=$UID .
+}
+
+run() {
+  docker run -it --rm -h $TAG -v $HOME:$HOME $TAG:$VER
+}
+
+usage() {
+  echo "usage: ${0##*/} <build | run>"
+  echo
+  exit
+}
+
+case $1 in
+  build) shift; build "$@";;
+  run) shift; run "$@";;
+  *) usage;;
+esac
