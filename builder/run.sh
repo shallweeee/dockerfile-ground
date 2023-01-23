799cf17cf9906afe92d77246ce673e4a7cc243e7
Heechan Park <phcphc82@gmail.com>
2023-01-23 15:17:05 +0900

    virt-viewer 빌드 환경 추가(미완성)

--- builder/run.sh
+++ builder/run.sh
@@ -7,7 +7,27 @@ MAKE_OPTIONS="-j16"
 IMAGE=builder
 WORKDIR=/build
 SCRIPT=build.sh
-SRC=src
+#SRC=src
+SRC=virt-viewer
+
+build_mp4box() {
+  cat << EOF > $SCRIPT
+#! /bin/bash
+"\$@"
+./configure $CONFIG_OPTIONS && make $MAKE_OPTIONS
+EOF
+}
+
+build_virt_viewer() {
+  cat << EOF > $SCRIPT
+#! /bin/bash
+apt update
+apt install ninja-build python3-pip libglib2.0-dev libgtk-3-dev libxml2-dev gettext
+pip install meson
+meson setup builddir
+meson compile -C builddir
+EOF
+}
 
 if [ -d $SRC ]; then
   (cd $SRC; git pull)
@@ -15,12 +35,9 @@ else
   git clone $GIT_REPOSITORY $SRC
 fi
 
-cat << EOF > $SCRIPT
-#! /bin/bash
-"\$@"
-./configure $CONFIG_OPTIONS && make $MAKE_OPTIONS
-EOF
+build_virt_viewer
 chmod u+x $SCRIPT
 
-docker run --rm -it -w $WORKDIR -v $PWD/$SRC:$WORKDIR -v $PWD/$SCRIPT:/usr/bin/$SCRIPT $IMAGE $SCRIPT "$@"
-rm -f $SCRIPT
+#docker run --rm -it -w $WORKDIR -v $PWD/$SRC:$WORKDIR -v $PWD/$SCRIPT:/usr/bin/$SCRIPT $IMAGE $SCRIPT "$@"
+#rm -f $SCRIPT
+docker run --rm -it -w $WORKDIR -v $PWD/$SRC:$WORKDIR -v $PWD/$SCRIPT:/usr/bin/$SCRIPT $IMAGE bash
